a4d8daaa50af28c49824eeaf2e291193af870716
Miledi75 <mileto.di.marco@howest.be>
2023-03-01 11:33:30 +0100

    Update to .Net6

--- cu.ApiBasics.Lesvoorbeeld.Avond.Api/Program.cs
+++ cu.ApiBasics.Lesvoorbeeld.Avond.Api/Program.cs
@@ -1,26 +1,44 @@
-using Microsoft.AspNetCore.Hosting;
+using cu.ApiBAsics.Lesvoorbeeld.Avond.Core.Interfaces.Repositories;
+using cu.ApiBAsics.Lesvoorbeeld.Avond.Core.Interfaces.Services;
+using cu.ApiBAsics.Lesvoorbeeld.Avond.Core.Services;
+using cu.ApiBasics.Lesvoorbeeld.Avond.Infrastructure.Data;
+using cu.ApiBasics.Lesvoorbeeld.Avond.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace cu.ApiBasics.Lesvoorbeeld.Avond.Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDatabase")));
+
+//repositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+//services
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
